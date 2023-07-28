f08bf928ea42a4b9c16403039ed7f9bbcf8b452c
realfraze <realfraze@protonmail.ch>
2023-07-28 09:43:57 +0200

    made readme

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,5 @@
 package org.example;
 
-
-/*
- * 1. Create project
- * 2. Make code that prints
- *   Hello, my name is
- *   yourname
- * but split every word in a single statement.
- * 3. Push the results on GitHub (remember to grant me access if you make it private).
- * 4. Link me the repo (duh).
- */
 public class Main {
 	public static void main(String[] args) {
 		System.out.print("Hello, ");
