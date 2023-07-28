9dbd9c578e2c706560d945c404304edb85ad2566
realfraze <realfraze@protonmail.ch>
2023-07-28 09:41:10 +0200

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+/*
+ * 1. Create project
+ * 2. Make code that prints
+ *   Hello, my name is
+ *   yourname
+ * but split every word in a single statement.
+ * 3. Push the results on GitHub (remember to grant me access if you make it private).
+ * 4. Link me the repo (duh).
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.print("Hello, ");
+		System.out.print("my ");
+		System.out.print("name ");
+		System.out.println("is:");
+		System.out.println("Fraaz");
+	}
+}
